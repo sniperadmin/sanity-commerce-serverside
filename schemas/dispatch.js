e025bb906ea87917838390f844e9211767798670
Nasr Galal <nasrbue@gmail.com>
2021-09-17 13:15:00 +0200

    feat: add setup

---
+++ schemas/dispatch.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'dispatch',
+  title: 'Dispatcher',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+    }
+  ]
+}
